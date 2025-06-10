fe7e56140f9f02bfaa365d5803cb65caff87f79a
c0a242365e <c0a242365e@edu.teu.ac.jp>
2025-06-10 16:13:54 +0900

    練習10 押下キーに応じたこうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -11,20 +11,29 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像のSurface
     bg_img2 = pg.transform.flip(bg_img,True,False)
-
     kk_image = pg.image.load("fig/3.png")
     kk_image = pg.transform.flip(kk_image,True,False)
+    kk_rct = kk_image.get_rect() #Rect取得
+    kk_rct.center = 300,200   #centerに初期座標設定
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
+        key_lst = pg.key.get_pressed() #キーの押下状態の取得
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
         x=tmr%3200
         # screen.blit(kk_image, [300, 200])
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kk_image, [300, 200]) #背景を先に張り付ける
+        screen.blit(kk_image,kk_rct) #Rect blit
         pg.display.update()
         tmr += 1        
         clock.tick(200)
