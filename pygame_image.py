4c2ddf4c11861202a24bcc79e56ea9ae6d1e4530
c0a242365e <c0a242365e@edu.teu.ac.jp>
2025-06-10 15:42:21 +0900

    練習7 背景画像追加

--- pygame_image.py
+++ pygame_image.py
@@ -19,6 +19,7 @@ def main():
         
         # screen.blit(kk_image, [300, 200])
         screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [-tmr+1600, 0])
         screen.blit(kk_image, [300, 200]) #背景を先に張り付ける
         pg.display.update()
         tmr += 1        
